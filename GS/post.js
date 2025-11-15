a2ecb28ffd7f86bdc31687c5377f1165a0f48033
yaoleksa <mryaremchyk@gmail.com>
2025-11-15 12:21:14 +0200

    continue updating integration logic

--- GS/post.js
+++ GS/post.js
@@ -1,7 +1,15 @@
-function doPost(e) {
+function doPost(event) {
   const whatsApp = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("WhatsApp");
-  const lastFilledRow = Math.max(...whatsApp.getRange("A:A").getValues()) ?
-  Math.max(...whatsApp.getRange("A:A").getValues()) + 1 : 2;
-  whatsApp.getRange(`E${lastFilledRow}`).setValue(JSON.stringify(e));
+  const lastFilledRow = whatsApp.getRange("A:A").getValues().filter(e => e != '').length + 1;
+  const data = JSON.parse(event.postData.contents);
+  whatsApp.getRange(`A${lastFilledRow}:E${lastFilledRow}`).setValues([
+    [
+      lastFilledRow,
+      Date(data.time),
+      data.from,
+      data.to,
+      data.body.toString()
+    ]
+  ]);
   return ContentService.createTextOutput("Success!");
 }
--- app.js
+++ app.js
@@ -1,7 +1,11 @@
+// Make nvironmental variables avaliable
+require('dotenv').config();
+
+// Enable required packages
 const { Client } = require('whatsapp-web.js');
 const { default: axios } = require('axios');
 const client = new Client();
-
+const ID = 'AKfycbzLKiYidUljg7P8u7aWd8Myd2y3TLsKHZhT3lbldDjKvh3In-0ghxSuL2rQkOQGzpnA';
 client.once('ready', () => {
     console.log('is ready');
 });
@@ -11,7 +15,10 @@ client.on('qr', (qr) => {
 });
 
 client.on('message_create', (msg) => {
-    axios.post('https://script.google.com/macros/s/AKfycbzi4gUi2de_5F4JFbZLwttFzdcqnnmOtrQl98SVVa06E_pMibD7U6t9uMCLN_izZzAB/exec', {
+    axios.post(`https://script.google.com/macros/s/${process.env.ACTIVATION_ID}/exec`, {
+        'time': msg.timestamp,
+        'from': msg.from,
+        'to': msg.to,
         'body': msg.body
     }, {
         'Content-Type': 'application/json',
